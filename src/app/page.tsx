d28538a50a37906680d780de572a8d5e7c521c75
Oleg Zapara <ole9zp@gmail.com>
2025-04-06 15:51:14 +0300

    initial project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid items-center justify-center min-h-screen">
+      <main className="text-3xl font-bold">
+        KPI Trade 
+      </main>
+    </div>
+  );
+}
