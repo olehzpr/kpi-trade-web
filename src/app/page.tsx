72e48484a2af1db1ac5c343086ae0363582dd52d
Oleg Zapara <ole9zp@gmail.com>
2025-04-06 15:53:08 +0300

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid items-center justify-center min-h-screen">
-      <main className="text-3xl font-bold">
-        KPI Trade 
-      </main>
+      <main className="text-3xl font-bold">KPI Trade</main>
     </div>
   );
 }
