0a469ba4328679aa1dc8953355e456b533ab3f00
Oleg Zapara <ole9zp@gmail.com>
2025-04-20 02:52:33 +0300

    fix: actual kpi-trade styles

---
+++ src/app/products/create/page.tsx
@@ -0,0 +1,3 @@
+export default function CreateProductPage() {
+  return <div>On this page you can create some kind of product</div>;
+}
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Button } from "../ui/button";
 import KpiTradeLogo from "@/components/ui/logo";
 import Link from "next/link";
+import { PlusIcon } from "lucide-react";
 
 export default function AppHeader() {
   return (
-    <div className="w-full p-2 flex justify-center shadow-md">
+    <div className="w-full p-2 flex justify-center bg-white shadow-sm shadow-neutral-200 sticky top-0 z-50">
       <div className="container flex justify-between items-center">
         <div className="flex items-center gap-2">
           <Link href="/">
@@ -13,7 +14,11 @@ export default function AppHeader() {
           </Link>
         </div>
         <div className="flex items-center gap-2">
-          <Button variant="secondary">Увійти</Button>
+          <Link href="/products/create">
+            <Button className="cursor-pointer">
+              Додати товар <PlusIcon />
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
