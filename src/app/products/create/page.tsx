16aa0bd18dc2b32bee650a45f3f6f9dd4ea53ed1
Oleg Zapara <ole9zp@gmail.com>
2025-04-20 19:58:30 +0300

    feat: create product page

--- src/app/products/create/page.tsx
+++ src/app/products/create/page.tsx
@@ -2,7 +2,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { Toggle } from "@/components/ui/toggle";
 import { Switch } from "@/components/ui/switch";
 
 export default function CreateProductPage() {
