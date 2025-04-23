86ee018263ac25d9d9de24c66f02d076b59415d8
Oleg Zapara <ole9zp@gmail.com>
2025-04-23 11:23:11 +0300

    feat: selectable images

--- src/components/products/image-gallery.tsx
+++ src/components/products/image-gallery.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import { ProductImage } from "@/types/products/product-image";
+import { useState } from "react";
+import { cx } from "class-variance-authority";
 
 export default function ImageGallery({ images }: { images: ProductImage[] }) {
+  const [selectedImage, setSelectedImage] = useState<number>(0);
   return (
     <>
       <div className="space-y-4">
         {images.length > 0 ? (
           <div className="aspect-square relative rounded-lg overflow-hidden bg-gray-100">
             <Image
-              src={images[0].url}
+              src={images[selectedImage].url}
               alt="Product image"
               fill
               className="object-cover"
@@ -20,13 +23,16 @@ export default function ImageGallery({ images }: { images: ProductImage[] }) {
           </div>
         )}
 
-        {/* Thumbnail gallery */}
         {images.length > 1 && (
           <div className="grid grid-cols-5 gap-2">
-            {images.map((image) => (
+            {images.map((image, index) => (
               <div
                 key={image.id}
-                className="aspect-square relative rounded overflow-hidden bg-gray-100"
+                className={cx(
+                  "aspect-square relative rounded overflow-hidden bg-gray-100 transition-all",
+                  index == selectedImage && "ring-2 ring-blue-600",
+                )}
+                onClick={() => setSelectedImage(index)}
               >
                 <Image
                   src={image.url}
